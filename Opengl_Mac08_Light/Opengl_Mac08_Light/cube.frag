dcac547f94f82c5b9cd671fa656e72ffafc8ebd3
yujunzhen <yujunzheno@163.com>
2018-08-03 19:10:11 +0800

    提交效果不一致奇怪了

--- Opengl_Mac08_Light/Opengl_Mac08_Light/cube.frag
+++ Opengl_Mac08_Light/Opengl_Mac08_Light/cube.frag
@@ -1,8 +1,25 @@
 #version 410 core
-uniform vec3 objectColor;
-uniform vec3 lightColor;
 out vec4 color;
+
+in vec3 Normal;
+in vec3 FragPos;
+
+uniform vec3 lightPos;
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+
 void main()
 {
-    color = vec4(lightColor * objectColor, 1.0f);
-}
+    // Ambient
+    float ambientStrength = 0.6f;
+    vec3 ambient = ambientStrength * lightColor;
+    
+    // Diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos -FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+    
+    vec3 result = (ambient + diffuse) * objectColor;
+    color = vec4(result, 1.0f);
+} 
--- Opengl_Mac08_Light/Opengl_Mac08_Light/main.cpp
+++ Opengl_Mac08_Light/Opengl_Mac08_Light/main.cpp
@@ -18,90 +18,113 @@
 #include <GLFW/glfw3.h>
 #include <time.h>
 #include <unistd.h>
-
+#include "Camera.cpp"
 GLuint vao,vbo;
 GLuint lightVao,lightVbo;
 GLuint program, LightPorgram;
+const GLuint WIDTH = 800, HEIGHT = 600;
+Camera camera(vmath::vec3(0.0f, 2.0f, 10.0f));
+GLfloat deltaTime = 0.0f;    // Time between current frame and last frame
+GLfloat lastFrame = 0.0f;      // Time of last frame
+GLfloat lastX  =  WIDTH  / 2.0;
+GLfloat lastY  =  HEIGHT / 2.0;
+bool keys[1024];
+vmath::vec3 lightPos(0.0f, 0.0f, 0.0f);
+
 void Init(){
     GLfloat vertices[] = {
-        -0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f,  0.5f, -0.5f,
-        0.5f,  0.5f, -0.5f,
-        -0.5f,  0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-
-        -0.5f, -0.5f,  0.5f,
-        0.5f, -0.5f,  0.5f,
-        0.5f,  0.5f,  0.5f,
-        0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
-        -0.5f, -0.5f,  0.5f,
-
-        -0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
-
-        0.5f,  0.5f,  0.5f,
-        0.5f,  0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f,  0.5f,
-        0.5f,  0.5f,  0.5f,
-
-        -0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f,  0.5f,
-        0.5f, -0.5f,  0.5f,
-        -0.5f, -0.5f,  0.5f,
-        -0.5f, -0.5f, -0.5f,
-
-        -0.5f,  0.5f, -0.5f,
-        0.5f,  0.5f, -0.5f,
-        0.5f,  0.5f,  0.5f,
-        0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f, -0.5f
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+        
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+        
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
     };
     ShaderInfo shaders[] = {{GL_VERTEX_SHADER, "cube.vert"},{GL_FRAGMENT_SHADER, "cube.frag"},{GL_NONE, NULL}};
      ShaderInfo shadersLight[] = {{GL_VERTEX_SHADER, "cubeLight.vert"},{GL_FRAGMENT_SHADER, "cubeLight.frag"},{GL_NONE, NULL}};
     program = LoadShaders(shaders);
     LightPorgram = LoadShaders(shadersLight);
-    glUseProgram(program);
-    glUseProgram(LightPorgram);
+    
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
     
     glGenBuffers(1,&vbo);
     glBindBuffer(GL_ARRAY_BUFFER,vbo);
     glBufferData(GL_ARRAY_BUFFER,  sizeof(vertices), vertices,GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),(GLvoid *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),(GLvoid *)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),(GLvoid *)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(0);
     glBindVertexArray(0);
 
     glGenVertexArrays(1, &lightVao);
     glBindVertexArray(lightVao);
     glBindBuffer(GL_ARRAY_BUFFER,vbo);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),(GLvoid *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),(GLvoid *)0);
     glEnableVertexAttribArray(0);
     glBindVertexArray(0);
 
 }
+void do_movement()
+{
+    
+}
 void Display(){
+    GLfloat currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+    
+    // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+    do_movement();
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUseProgram(program);
+
     GLint objectColorLoc = glGetUniformLocation(program, "objectColor");
     GLint lightColorLoc  = glGetUniformLocation(program, "lightColor");
+    GLint lightPosLoc    = glGetUniformLocation(program, "lightPos");
     glUniform3f(objectColorLoc, 1.0f, 0.5f, 0.31f);
-    glUniform3f(lightColorLoc,  1.0f, 0.5f, 1.0f);
+    glUniform3f(lightColorLoc,  1.0f, 2.0f, 1.0f);
+    glUniform3f(lightPosLoc,    lightPos[0], lightPos[1], lightPos[2]);
 
     // Create camera transformations
     vmath::mat4 view = vmath::mat4::identity();
-
-    vmath::mat4 projection = vmath::mat4::identity();
+    view = camera.GetViewMatrix();
+    vmath::mat4 projection = vmath::perspective(camera.Zoom, 1.0, 0.1f, 100.0f);
     // Get the uniform locations
     GLint modelLoc = glGetUniformLocation(program, "model");
     GLint viewLoc  = glGetUniformLocation(program,  "view");
@@ -113,6 +136,8 @@ void Display(){
     // Draw the container (using container's vertex attributes)
     glBindVertexArray(vao);
     vmath::mat4 model = vmath::mat4::identity();
+
+  
     glUniformMatrix4fv(modelLoc, 1, GL_FALSE, model);
     glDrawArrays(GL_TRIANGLES, 0, 36);
     glBindVertexArray(0);
@@ -120,14 +145,19 @@ void Display(){
     // Also draw the lamp object, again binding the appropriate shader
 
     // Get location objects for the matrices on the lamp shader (these could be different on a different shader)
+//    glUseProgram(LightPorgram);
+//
 //    modelLoc = glGetUniformLocation(LightPorgram, "model");
 //    viewLoc  = glGetUniformLocation(LightPorgram, "view");
 //    projLoc  = glGetUniformLocation(LightPorgram, "projection");
 //    // Set matrices
+//
 //    glUniformMatrix4fv(viewLoc, 1, GL_FALSE,view);
 //    glUniformMatrix4fv(projLoc, 1, GL_FALSE,projection);
+//
 //    model = vmath::translate(0.5f, 0.5f, 0.5f) * model;
-//    model = vmath::scale(vmath::vec3(3.0f)) * model; // Make it a smaller cube
+//    model = model * vmath::scale(vmath::vec3(0.4f)) ; // Make it a smaller cube
+//
 //    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, model);
 //    // Draw the light object (using light's vertex attributes)
 //    glBindVertexArray(lightVao);
@@ -137,15 +167,60 @@ void Display(){
      glutPostRedisplay();
 }
 
+void SpacialKeyBoardFunc(int key, int width, int height){
+    switch (key) {
+        case GLUT_KEY_UP:
+            camera.ProcessKeyboard(FORWARD, deltaTime);
+
+            break;
+        case GLUT_KEY_DOWN:
+            camera.ProcessKeyboard(BACKWARD, deltaTime);
+
+            break;
+        case GLUT_KEY_LEFT:
+            camera.ProcessKeyboard(LEFT, deltaTime);
+
+            break;
+        case GLUT_KEY_RIGHT:
+            camera.ProcessKeyboard(RIGHT, deltaTime);
+
+            break;
+            
+        default:
+            break;
+    }
+    // Camera controls
+    
+}
+bool firstMouse = true;
+void mouse_callback(int button, int state, int xpos, int ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+    
+    GLfloat xoffset = xpos - lastX;
+    GLfloat yoffset = lastY - ypos;  // Reversed since y-coordinates go from bottom to left
+    
+    lastX = xpos;
+    lastY = ypos;
+    
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
 int main(int argc, char *argv[]){
     glfwInit();
     glutInit(&argc, (char**)argv);
     glutInitDisplayMode(GLUT_3_2_CORE_PROFILE | GLUT_RGBA | GLUT_DOUBLE | GLUT_MULTISAMPLE | GLUT_STENCIL  );
-    glutInitWindowSize(512, 512);
+    glutInitWindowSize(WIDTH, HEIGHT);
     glutCreateWindow(argv[0]);
     glewExperimental = GL_TRUE;
     GLenum err = glewInit();
     glEnable(GL_DEPTH_TEST);
+    glutSpecialFunc(&SpacialKeyBoardFunc);
+    glutMouseFunc(&mouse_callback);
     if (GLEW_OK != err) {
         fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
     }
